d9e224850f282d63f208206e0f45a0f399e5602c
Varun Madhukara <varun@Varuns-MacBook-Pro.local>
2019-07-01 22:09:36 +0100

    Extending TestRunner class and including it in testng.xml

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -4,12 +4,13 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/java/features",
 		glue = "src/test/java/stepDefinitions")
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
 	
 }
